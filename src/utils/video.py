74d815fda3f799679db1ce736798de0530bde044
Ilya Korotkov <i59korotkov@gmail.com>
2023-04-22 09:07:36 +0300

    Added docstrings

--- src/utils/video.py
+++ src/utils/video.py
@@ -5,11 +5,21 @@ import cv2
 
 class VideoIterator:
     def __init__(self, file_path: str) -> None:
+        """
+        Класс для итерирования по кадрам видеофайла
+
+        Args:
+            file_path: Путь к видеофайлу
+
+        Raises:
+            Exception:
+        """
         if not path.isfile(file_path):
-            raise 
+            raise Exception(f'\'{file_path}\' does not exist or is not a file')
 
         self.cap = cv2.VideoCapture(file_path)
 
+        # Get video properties
         self.length = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
         self.fps = int(self.cap.get(cv2.CAP_PROP_FPS))
         self.width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
