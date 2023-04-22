d0a725c639e6c089793e9db012865d5b888cbd8f
Ilya Korotkov <i59korotkov@gmail.com>
2023-04-22 09:45:10 +0300

    Added docs

---
+++ docs/_build/html/_sources/utils.rst.txt
@@ -0,0 +1,21 @@
+utils package
+=============
+
+Submodules
+----------
+
+utils.video module
+------------------
+
+.. automodule:: utils.video
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: utils
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ docs/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.append(os.path.abspath('../src'))
+
+project = 'Censorship'
+copyright = '2023, IKBO-04-20'
+author = 'IKBO-04-20'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
